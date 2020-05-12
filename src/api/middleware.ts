b3e67195c0a3dae7f09a88c9a245e3c5912b7889
Sam Jamal <sammy@ultra.me>
2020-05-11 21:47:26 -0700

    start prep for deploymetn

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -1,14 +1,14 @@
-import { createProxyMiddleware } from "http-proxy-middleware";
+// import { createProxyMiddleware } from "http-proxy-middleware";
 
 export default (app: any) => {
-  app.use(
-    createProxyMiddleware("/api-users", {
-      target: "http://localhost:8090/"
-    })
-  );
-  app.use(
-    createProxyMiddleware("/api-events", {
-      target: "http://localhost:4433/"
-    })
-  );
+  // app.use(
+  //   createProxyMiddleware("/api-users", {
+  //     target: "http://localhost:8090/"
+  //   })
+  // );
+  // app.use(
+  //   createProxyMiddleware("/api-events", {
+  //     target: "http://localhost:4433/"
+  //   })
+  // );
 };
