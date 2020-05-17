8f4300c6cd55e3c8411360604988607840cacb0a
Sam Jamal <sammy@ultra.me>
2020-05-16 20:08:16 -0700

    adjust build folder location in serverfile

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static("build"));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
 app.listen(8080, function (err) {
